22d6b6a21001a48c216a4c553562df99bb7b25e5
root <root@DESKTOP-HMD4H4K.localdomain>
2021-02-16 18:15:44 -0500

    added ignore for tf lock.hcl (new for .13?)

---
+++ deployment.tf
@@ -0,0 +1,14 @@
+### Metadata
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-1"
+}
+
